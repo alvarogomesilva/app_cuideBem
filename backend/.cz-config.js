0d13528f97632a61ccc5457c87aede9a8fa41c53
LeoGomes0 <leonardobrgomes@outlook.com>
2024-02-01 14:32:23 -0300

    build: adicionando biblioteca commitizen
    O commitizen é uma biblioteca utilizada para automatizar os commits semânticos, assim, facilitando
    na hora da produção e envios de commits.

---
+++ backend/.cz-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    types: [
+      { value: 'feat', name: 'feat:     Adicionar uma nova feature' },
+      { value: 'fix', name: 'fix:      Corrigir um bug' },
+      { value: 'chore', name: 'chore:    Alterações que não são de código' },
+      { value: 'docs', name: 'docs:     Alterações na documentação' },
+      { value: 'style', name: 'style:    Mudanças que não afetam o significado do código (espaços em branco, formatação, etc.)' },
+      { value: 'refactor', name: 'refactor: Refatoração de código' },
+      { value: 'test', name: 'test:     Adição ou modificação de testes' },
+    ],
+  };
+  
