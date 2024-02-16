9c4900e5cc295ff23790b528ef920e5dd78d4ed1
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-02-16 11:01:22 -0300

    Organizando a tela de login/Cuidador

--- frontend/src/screens/Caregiver/LoginCaregiver/index.jsx
+++ frontend/src/screens/Caregiver/LoginCaregiver/index.jsx
@@ -1,12 +1,10 @@
 import { styles } from './styles'
 
-import { KeyboardAvoidingView, Platform, SafeAreaView, Text, View } from "react-native";
+import { Image, KeyboardAvoidingView, Platform, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import { Feather } from '@expo/vector-icons';
 
 import Logo from '../../../components/Logo'
-import Input from "../../../components/Input";
-import Submit from "../../../components/Submit";
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../../contexts/AuthContext';
 
@@ -16,42 +14,81 @@ export default function LoginCaregiver() {
     const { signIn, loadingAuth } = useContext(AuthContext)
 
     return (
-        <View style={styles.container}>
-            <LinearGradient
-                style={styles.container}
-                start={[0.2, 1]}
-                end={[0.8, 0.1]}
-                colors={[ '#C4E1FF','#C4E1FF', '#FFF']}>
-                <Logo />
-                <KeyboardAvoidingView style={styles.content} behavior={Platform.OS === 'ios' ? 'padding': ''}>
+      
+            <View style={styles.container}>
+                <LinearGradient
+                    colors={['#ADCAE8', '#B8D5F3', '#C4E1FF', '#FFFFFF']}
+                    style={styles.gradient}>
 
-                    <Input 
-                        placeholder='Digite seu email' 
-                        autoCapitalize='none'
-                        value={email}
-                        onChangeText={setEmail}
-                    >
-                        <Feather name="mail" style={styles.icon} />
-                    </Input>
+                    <SafeAreaView style={styles.safeAreaView}>
+                        <Image
+                            source={require('../../../../assets/caregivers.png')}
+                            style={styles.image}
+                        />
 
-                    <Input 
-                        placeholder='Digite sua senha'
-                        secureTextEntry={true}
-                        value={password}
-                        onChangeText={setPassword}
-                    >
-                        <Feather name="lock" style={styles.icon} />
-                    </Input>
+                    </SafeAreaView>
+                </LinearGradient>
 
-                    <Submit 
-                        loadingAuth={loadingAuth}
-                        onPress={() => signIn(email, password)}
-                        text='Login'
-                    />
-                </KeyboardAvoidingView>
-            </LinearGradient>
-        </View>
+                <View style={styles.content}>
+                    <Text style={styles.title}>Cuidadores</Text>
+                    <View style={styles.boxInputs}>
+                        <View style={styles.box}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Digite seu email'
+                            />
+                            <Feather name="mail" style={styles.icon} />
+                        </View>
+                        <View style={styles.box}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Digite sua senha'
+                            />
+                            <Feather name="lock" style={styles.icon} />
+                        </View>
 
+                        <TouchableOpacity style={styles.submit} activeOpacity={0.9}>
+                            <Text style={styles.submitText}>Entrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
     )
 }
+
+{/* <View style={styles.container}>
+<LinearGradient
+    style={styles.container}
+    start={[0.2, 1]}
+    end={[0.8, 0.1]}
+    colors={[ '#C4E1FF','#C4E1FF', '#FFF']}>
+    <Logo />
+    <KeyboardAvoidingView style={styles.content} behavior={Platform.OS === 'ios' ? 'padding': ''}>
+
+        <Input 
+            placeholder='Digite seu email' 
+            autoCapitalize='none'
+            value={email}
+            onChangeText={setEmail}
+        >
+            <Feather name="mail" style={styles.icon} />
+        </Input>
+
+        <Input 
+            placeholder='Digite sua senha'
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+        >
+            <Feather name="lock" style={styles.icon} />
+        </Input>
+
+        <Submit 
+            loadingAuth={loadingAuth}
+            onPress={() => signIn(email, password)}
+            text='Login'
+        />
+    </KeyboardAvoidingView>
+</LinearGradient>
+</View> */}
 
--- frontend/src/screens/Caregiver/LoginCaregiver/styles.js
+++ frontend/src/screens/Caregiver/LoginCaregiver/styles.js
@@ -1,28 +1,82 @@
 import { StyleSheet } from "react-native";
-import { neutral } from '../../../constants/colors'
+import Colors from '../../../constants/colors'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        backgroundColor: Colors.white
+    },
+
+    gradient: {
+        flex: 2,
+    },
+
+    safeAreaView: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    image: {
+        width: wp(45),
+        height: hp(25),
+    },
+
+    content: {
+        flex: 2,
+        marginTop: 15,
+        backgroundColor: Colors.white
     },
 
     title: {
         textAlign: 'center',
-        fontSize: 25,
-        marginVertical: 10
+        fontSize: hp(3.5),
+        fontWeight: "700",
+        color: Colors.quinary,
+        marginBottom: 15
     },
 
-    content: {
+    boxInputs: {
         marginHorizontal: 15,
+    },
+
+    box: {
+        position: 'relative',
+        marginVertical: 10,
+        height: hp(6.5)
     },
 
     icon: {
         position: 'absolute',
-        color: neutral,
-        fontSize: 20,
-        top: 15,
+        color: '#e0e0e0',
+        top: 17,
         left: 15,
-        zIndex: 2
+        fontSize: hp(2)
+    },
+
+    input: {
+        height: '100%',
+        borderWidth: 2,
+        borderColor: '#e0e0e0',
+        borderRadius: 10,
+        fontSize: hp(2.5),
+        paddingLeft: 40
+    },
+
+    submit: {
+        height: hp(8),
+        backgroundColor: Colors.quaternary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginTop: 10
+    },
+
+    submitText: {
+        color: Colors.white,
+        fontSize: hp(2.5)
     }
+
+
 })
